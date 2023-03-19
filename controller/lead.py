50ef1e04a6a59d97eff66bca212063e368eef495
Vinoth Murugan <vinoth4murugan@gmail.com>
2023-03-19 11:34:09 +0000

    Lead feature complete
    * Delete lead functionality done and tested.

--- controller/lead.py
+++ controller/lead.py
@@ -5,10 +5,14 @@
 def create_lead():
 	print 'Lead creation done.'
 
-	# update lead.
+# update lead.
 	def update_lead():
 		print 'lead update doen.'
 
-		# view lead.
+# view lead.
 		def get_lead():
 			print 'Get lead done.'
+
+# delete lead query.
+def delete_lead_query():
+	print 'lead deleted from database'
