fc0c1d127886ec9577f2a5554f8ea5c866868ecd
Vinoth Murugan <vinoth4murugan@gmail.com>
2023-03-19 11:50:31 +0000

    Lead feature
    * full lead feature done and commited.

---
+++ controller/lead.py
@@ -0,0 +1,18 @@
+# Lead contrailer implementation.
+
+
+# create lead.
+def create_lead():
+	print 'Lead creation done.'
+
+# update lead.
+	def update_lead():
+		print 'lead update doen.'
+
+# view lead.
+		def get_lead():
+			print 'Get lead done.'
+
+# delete lead query.
+def delete_lead_query():
+	print 'lead deleted from database'
