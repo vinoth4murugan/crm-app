1ebaec16028d7339381c240c9a2dca1fecb4e780
vinoth4murugan <127675327+vinoth4murugan@users.noreply.github.com>
2023-03-19 12:02:11 +0000

    Lead feature (#1)
    * full lead feature done and commited.

---
+++ model/lead.py
@@ -0,0 +1,14 @@
+# Lead model implementation.
+
+
+# Insert lead query.
+def insert_lead():
+	print 'Lead inserted into database.'
+
+# Update lead query.
+def update_lead_query():
+	print 'Lead updated into datbase.'
+
+	# get lead query.
+	def select_lead_query():
+		print 'Lead select from database.'
