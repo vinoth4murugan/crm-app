9ec1fb059dbb1aaefef8d01ef25b29349803779c
Vinoth Murugan <vinoth4murugan@gmail.com>
2023-03-19 11:24:43 +0000

    Lead feature in progress
    * update lead and get lead funtionality done.

--- controller/lead.py
+++ controller/lead.py
@@ -4,4 +4,11 @@
 # create lead.
 def create_lead():
 	print 'Lead creation done.'
-	
+
+	# update lead.
+	def update_lead():
+		print 'lead update doen.'
+
+		# view lead.
+		def get_lead():
+			print 'Get lead done.'
